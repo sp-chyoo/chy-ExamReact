61a3ae5c7e9386d5003973b466c6bfc14a58d5bf
sp-chyoo <chyoo@spesys.co.kr>
2023-04-05 11:19:05 +0900

    카운터 컴포넌트 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+/**
+ * 컴포넌트 함수
+ * @returns 버튼과 숫자를 전달   
+ */
+function Counter(){
+  return <div>
+    <button>+</button>0
+  </div>
+}
+
 function App() {
   return (
     <div>
-
+      <Counter></Counter>
     </div>
   );
 }
