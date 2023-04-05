c2af7c28b9c1bab33be6a0a1191da1d3e28bd9b5
sp-chyoo <chyoo@spesys.co.kr>
2023-04-05 16:14:47 +0900

    useState()를 이용한 다양한 처리 방식을 볼 수 있음

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import {useState} from 'react';
+
+const initialNames = () => {
+  console.log('call initialNames()');
+  return ['홍길동','김철수'];
+}
 
 function App() {
+
+  // 맨처음 렌더링 할때만 렌더링 하고 싶다면, 초기화 함수를 콜백 형태로 부르면 됨.
+  //const [names, setNames] = useState(['홍길동','김철수']);
+  const [names, setNames] = useState(()=> {
+    return initialNames();
+  });
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  }
+ 
+  const handleUpload = () => {
+    setNames((preState) => {
+      return [input, ...preState];
+    });
+  }
+
   return (
     <div>
-
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
     </div>
   );
 }
