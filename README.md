b77577ceedad4583277fabf82ed16f49ca1a4883
sp-chyoo <128347057+sp-chyoo@users.noreply.github.com>
2023-04-06 10:05:53 +0900

    Revert "Redux toolkit exam"

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-> 출처 : https://www.youtube.com/watch?v=9wrHxqI6zuM&t=714s
-- 기본 설치
-```console
-> npm install redux react-redux
-> npm install @reduxjs/toolkit
-```
-- React Redux Toolkit 설명 추가 및 소스 분석
-  - configureStore로 store를 만듬   
-  - createSlice로 작은 store를 만듬   
-  - actionCreator 함수를 자동으로 만듬   
-  - immer.js가 내장 되어 있어서 불변하게 데이터를 처리하지 않아도 됨
-- 등장 이유
-  - 설정 복잡
-  - 미들웨어 설치도 복잡
-  - 반복되는 코드 많음
-  - 불변성 유지의 어려움
-- Store를 나누고 싶어짐
-  - 작은 스토어를 slice라고 함
+# chy-ExamReact
+React  연습 소스를 관리하기 위한 저장소
+- brunch 설명
+  - master : 연습 소스의 기초가 되는 소스를 관리
+  - dev : 마스터의 소스를 기초로 개발 소스를 관리
+  - feature : dev의 소스를 기초로 추가된 기능 소스를 관리
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,10 @@
 import './App.css';
-//import {createStore} from 'redux';
-import {Provider, useSelector, useDispatch} from 'react-redux';
-import store from './store';
-import {up} from './counterSlice';
-
-/**
- * 컴포넌트 함수
- * @returns 버튼과 숫자를 전달   
- */
-function Counter(){
-  const count = useSelector(state => {
-    return state.counter.value; // store의 reducer에 설정된 이름
-  });
-  const dispatch = useDispatch();
-
-  return <div>
-    <button onClick={() => {
-      //dispatch({type:'counter/up', payload:2});
-      dispatch(up(2));
-    }}
-    >+</button>{count}
-  </div>
-}
 
 function App() {
   return (
-    <Provider store={store}>
-      <div>
-        <Counter></Counter>
-      </div>
-    </Provider>
-    
+    <div>
+
+    </div>
   );
 }
 
