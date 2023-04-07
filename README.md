7e0c06f5df3c72895161f9657e0c4f02b183c88d
sp-chyoo <chyoo@spesys.co.kr>
2023-04-07 10:12:05 +0900

    기초 소스 추가 1. 버튼 클릭 시 1씩 증가

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# chy-ExamReact
-React  연습 소스를 관리하기 위한 저장소
-- brunch 설명
-  - master : 연습 소스의 기초가 되는 소스를 관리
-  - dev : 마스터의 소스를 기초로 개발 소스를 관리
-  - feature : dev의 소스를 기초로 추가된 기능 소스를 관리
+> ## 출처 : https://www.youtube.com/watch?v=VxqZrL4FLz8&list=PLZ5oZ2KmQEYjwhSxjB_74PoU6pmFzgVMO&index=3
+- 내용 정리 추가
+- 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increaseCountState = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
+      <p>State : {count}</p>
+      <button onClick={increaseCountState}>State 올려</button>
 
     </div>
   );
