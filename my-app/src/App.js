1120e3929ae8739bede9eda1ac4075a8743c4ce8
sp-chyoo <chyoo@spesys.co.kr>
2023-04-07 11:20:40 +0900

    - useRef() 와 DOM 요소를 연결하는 예제 추가.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {useEffect, useRef} from 'react';
 
 function App() {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  },[]);
+
   return (
     <div>
-
+      <input ref={inputRef} type="text" placeholder="user name"></input>
+      <button>로그인</button>
     </div>
   );
 }
