31414ed078016638c4741a9ed8307a904c7cc9f0
sp-chyoo <chyoo@spesys.co.kr>
2023-04-10 10:19:34 +0900

    1. 객체 타입의 변수에 useEffect와 useMemo를 사용하는 예제 추가 - 객체 타입 변수를 useEffect에 사용 시 렌더링 마다 호출됨 - 주소값이 계속 변경되기 때문 - 그럴 때 useMemo를 이용하여 특정 조건에만 렌더링되도록 할 수 있음

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,37 @@
 import './App.css';
-import React, { useState, useMemo } from 'react';
-
-const hardCalculate = (number) => {
-  console.log('어려운 계산!');
-
-  for (let i = 0 ; i < 999999999; i++) {} // 생각하는 시간
-
-  return number + 10000;
-};
-
-const easyCalculate = (number) => {
-  console.log('짱 쉬운 계산!');
-
-  return number + 1;
-}
+import React, { useState, useMemo, useEffect } from 'react';
 
 function App() {
-  const [hardNumber, setHardNumber] = useState(1);
-  const [easyNumber, setEasyNumber] = useState(1);
 
-  //const hardSum = hardCalculate(hardNumber);
-  const hardSum = useMemo(() => {
-    return hardCalculate(hardNumber)
-  }, [hardNumber]);
-  const easySum = easyCalculate(easyNumber);
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
+
+  const location = useMemo(()=>{
+    return {
+      country: isKorea ? '한국' : '외국',
+    }
+  }, [isKorea]);
+  
+  // {
+  //   country: isKorea ? '한국' : '외국',
+  // };
+
+  useEffect(() => {
+    console.log("useEffect 호출");
+  },[location]);
 
   return (
     <div>
-      <h3>어려운 계산기</h3>
+      <h2>하루에 몇끼 먹어요?</h2>
       <input
         type="number"
-        value={hardNumber}
-        onChange={(e) => setHardNumber(parseInt(e.target.value))}>
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}>
       </input>
-      <span> + 10000 = {hardSum}</span>
-
-      <h3>쉬운 계산기</h3>
-      <input
-        type="number"
-        value={easyNumber}
-        onChange={(e) => setEasyNumber(parseInt(e.target.value))}>
-      </input>
-      <span> + 1 = {easySum}</span>
+      <hr />
+      <h2>어느 나라에 있어요?</h2>
+      <p>나라 : {location.country}</p>
+      <button onClick={()=>setIsKorea(!isKorea)}>비행기 타자</button>
     </div>
   );
 }
