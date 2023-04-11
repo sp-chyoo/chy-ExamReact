ced399fe19906c19c33402059cd2368f10e495ef
sp-chyoo <chyoo@spesys.co.kr>
2023-04-11 13:19:40 +0900

    useCallBack  예제 소스 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,42 @@
 import './App.css';
-import { useEffect, useState, useCallback } from 'react';
+import { useCallback, useState } from 'react';
+import Box from './Box';
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [toggle, setToggle] = useState(true);
+  const [size, setSizes] = useState(100);
+  const [isDark, setIsDark] = useState(false);
 
-  // const someFunction = () => {
-  //   console.log(`someFunc: number : ${number}`);
-  //   return;
+  // 버튼 클릭 시 영향을 받지 않도록 수정
+  const createBoxStyle = useCallback(() => {
+    return {
+          backgroundColor: 'pink',
+          width: `${size}px`,
+          height: `${size}px`
+        };
+  }, [size]);
+
+  // const createBoxStyle = () => {
+  //   return {
+  //     backgroundColor: 'pink',
+  //     width: `${size}px`,
+  //     height: `${size}px`
+  //   };
   // };
-  const someFunction = useCallback(() => {
-      console.log(`someFunc: number : ${number}`);
-      return;
-  }, [number]);
-
-  useEffect(() => {
-    console.log('someFunction이 변경되었습니다.');
-  }, [someFunction]);
 
   return (
-    <div>
+    <div 
+      style={{
+        background: isDark ? 'black' : 'white' ,
+      }}
+    >
       <input
         type="number"
-        value={number}
-        onChange={(e) => {setNumber(e.target.value)}}>
-        </input>
-        <button onClick={() => {setToggle(!toggle)}}>{toggle.toString()}</button>
-        <br/>
-        <button
-          onClick={someFunction}
-        >
-          Call someFunc
-        </button>
+        value={size}
+        onChange={(e) => {setSizes(e.target.value)}}
+      >
+      </input>
+      <button onClick={() => setIsDark(!isDark)}>Change Theme</button>
+      <Box createBoxStyle={createBoxStyle}></Box>
     </div>
   );
 }
